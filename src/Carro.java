712e581ba72ac58958c479bcb154d750158b2844
juujacometti <julyajacometty@gmail.com>
2024-04-05 14:50:52 -0300

    início

---
+++ src/Carro.java
@@ -0,0 +1,50 @@
+public class Carro {
+    public static void main(String[] args) {
+
+// Exercício POO
+// OBJETIVO 1: Criar uma classe. / OBJETIVO 2: Criar atributos para a classe // OBJETIVO 3: Criar métodos
+
+    // Classe:
+        class carro {
+
+        //Atributos:
+            String cor;
+            String modelo;
+            int capacidadeTanque;
+
+        //Construtor:
+        // Métodos:
+
+            // Cor
+            void setCor(String cor) {
+                this.cor = cor;
+            }
+            String getCor() {
+                return cor;
+            }
+
+            // Modelo:
+            void setModelo (String modelo) {
+                this.modelo = modelo;
+            }
+            String getModelo () {
+                return modelo;
+            }
+
+            // Capacidade do tanque:
+            void setCapacidadeTanque (int capacidadeTanque) {
+                this.capacidadeTanque = capacidadeTanque;
+            }
+            int getCapacidadeTanque () {
+                return capacidadeTanque;
+            }
+
+            // Método do valor total para encher o tanque:
+            double valorTotalTanque (double valorCombustivel) {
+                return capacidadeTanque * valorCombustivel;
+
+            }
+        }
+
+    }
+}
